bc39ecff4865fd0853513dca0d283863c9b02bc4
Doug Headley <dheadl@ext.uber.com>
2020-12-02 12:31:46 -0800

    lowest_version_packages method

--- lib/yarnlock/entry/collection.rb
+++ lib/yarnlock/entry/collection.rb
@@ -22,6 +22,12 @@ module Yarnlock
         end
       end
 
+      def lowest_version_packages
+        each_with_object({}) do |entry, packages|
+          packages[entry.package] = [entry, packages[entry.package]].compact.min_by(&:version)
+        end
+      end
+
       def as_json(_options = {})
         each_with_object({}) do |entry, entries|
           entries.merge! entry.to_h
